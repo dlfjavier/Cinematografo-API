7cd3ff7005b5b65c6eceda8e9698dee758aa5418
dlfjavier <dlfjavier@gmail.com>
2023-08-04 16:05:08 -0300

    Actualizar main.py y datasets

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 app = FastAPI()
 
+@app.get("/peliculas_idioma/{Idioma}")
+def peliculas_idioma(Idioma}: str):
+    
 
 @app.get("/peliculas_duracion/{Pelicula}")
 def peliculas_duracion(Pelicula: str):
@@ -33,6 +36,42 @@ def franquicia(Franquicia: str):
     average_revenueF = "{:,.2f}".format(average_revenue)
     return f"La franquicia {Franquicia} posee {movies_count} películas, una ganancia total de {total_revenueF} y una ganancia promedio de {average_revenueF}"
 
+@app.get("/peliculas_pais/{Pais}")
+def peliculas_pais( Pais: str ): 
+    # Cargar el DataFrame desde el archivo 'pq_countries.parquet'
+    df_countries = pd.read_parquet('datasets/pq_countries.parquet')
+
+    # Buscar la fila correspondiente al país en el DataFrame
+    country_row = df_countries[df_countries['countries_keys'] == Pais]
+
+    if not country_row.empty:
+        Z = country_row['countries_keys'].values[0]
+        X = country_row['productions'].values[0]
+        Y = country_row['countries'].values[0]
+        mensaje = f"Se produjeron {X} películas en el país {Z} ({Y})"
+        return mensaje
+    else:
+        return "País no encontrado en el DataFrame"
+
+    
+def productoras_exitosas(Productora: str):
+    # Cargar el DataFrame desde el archivo 'pq_prodCo.parquet'
+    df_prodCo = pd.read_parquet('datasets/pq_prodCo.parquet')
+
+    # Filtrar el DataFrame para obtener las filas donde la columna 'companies' sea igual al valor de la productora
+    filtro = df_prodCo['companies'] == Productora
+    resultado = df_prodCo[filtro]
+
+    # Verificar si se encontraron resultados para la productora
+    if resultado.empty:
+        print(f"No se encontraron datos para la productora '{Productora}'")
+        return None
+    else:
+    # Obtener los valores de 'total_revenue' y 'total_movies' para la productora
+        total_revenue_resultado = "{:,.2f}".format(resultado['total_revenue'].values[0])
+        total_movies_resultado = resultado['total_movies'].values[0]
+
+        return f"La productora {Productora} ha tenido un revenue de {total_revenue_resultado} y ha realizado {total_movies_resultado} películas."
 
 @app.get("/get_director/{nombre_director}")
 def get_director(nombre_director: str):
@@ -82,3 +121,33 @@ def get_director(nombre_director: str):
     respuesta += "\n" + '\n'.join([', '.join(pelicula) for pelicula in detalles_peliculas_str])
     return respuesta
 
+@app.get("/recomendacion/{titulo}")
+def recomendacion(titulo: str):
+    # dlf title_version elegida
+    # Cargar el DataFrame desde el archivo pq_reccom.parquet
+    df = pd.read_parquet('datasets/pq_reccom.parquet')
+
+    # Reemplazar valores nulos en la columna de títulos con una cadena vacía
+    df["title"].fillna("", inplace=True)
+
+    # Crear una matriz TF-IDF de los títulos de las películas
+    tfidf = TfidfVectorizer(stop_words="english")
+    tfidf_matrix = tfidf.fit_transform(df["title"])
+
+    # Calcular la similitud coseno entre los títulos
+    similarity_matrix = cosine_similarity(tfidf_matrix)
+
+    # Obtener el índice de la película proporcionada
+    idx = df[df["title"] == titulo].index[0]
+
+    # Obtener los puntajes de similitud de la película proporcionada con todas las demás películas
+    similarity_scores = list(enumerate(similarity_matrix[idx]))
+
+    # Ordenar las películas según el puntaje de similitud en orden descendente
+    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+
+    # Obtener los títulos de las 5 películas más similares
+    top_movies = [df.iloc[score[0]]["title"] for score in similarity_scores[1:6]]
+
+    return top_movies
+    
