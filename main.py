099d1c7f47095dff33e35353094d70d299240aec
dlfjavier <dlfjavier@gmail.com>
2023-08-11 19:52:06 -0300

    Actualizar main.py y venv

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ def get_director(nombre_director: str):
 
 @app.get("/recomendacion/{titulo}")
 def recomendacion(titulo: str):
-    # Cargar el DataFrame desde el archivo pq_reccom.parquet
+    # Cargar el DataFrame desde el archivo pq_reccomCol.parquet
     df = pd.read_parquet('datasets/pq_reccomCol.parquet')
     tfidf = TfidfVectorizer(stop_words="english")
     df['title'] = df['title'].fillna("")
